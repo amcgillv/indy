00821f987f2060b02377df1ba28dd316f2b21094
Charlie Windolf <cwindolf95@gmail.com>
2017-01-31 19:32:55 -0500

    article box progress

--- app/controllers/issue_controller.rb
+++ app/controllers/issue_controller.rb
@@ -2,13 +2,6 @@ class IssueController < ApplicationController
 
     def toc
 		@issue = Issue.find(params[:id])
-        @title = @issue.title
-        @nav_issue = @issue
-        # so we can print articles by category in toc
-        @articles = @issue.articles.sort do |a, b| 
-        	cat = a.category <=> b.category
-        	cat == 0 ? a.title <=> b.title : cat # tiebreak with title
-        end
         @og_title, @title = @issue.title
         @og_image = "http://image.issuu.com/" + @issue.document_number + "/jpg/page_1.jpg" if !@issue.document_number.nil?
         @og_type = "book"
