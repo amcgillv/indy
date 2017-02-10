0a69efeff5accbe05fb6184a36a63e78a772689f
Charlie Windolf <cwindolf95@gmail.com>
2017-02-09 22:56:35 -0500

    little issue feed tweaks. getting close here

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -30,7 +30,7 @@ class HomeController < ApplicationController
         @feed_articles = Article.where("published_at < ?", @current_issue.date).limit(20)
 
         # paginate
-        issues_per_page = 20.0
+        issues_per_page = 10.0
         @pages = (@issues.length.to_f / issues_per_page).ceil
 
         page_num = 0
